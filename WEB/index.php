489b66234af2347ae195f1ee7ccd141c542035a7
boomix <Sandis444@inbox.lv>
2017-12-03 22:51:58 +0200

    Fixed issue that players in servers tab where not showing up
    Also fixed that in server page now it gets the latest player username, as before it was getting the first one

--- WEB/index.php
+++ WEB/index.php
@@ -11,27 +11,7 @@ if( strlen($_SERVER['REQUEST_URI']) > 1 && substr($_SERVER['REQUEST_URI'], -1) !
      exit;
 }
 
-include 'config.php';
-
-//Some other configs
-define("TIMEFORMAT", "Y-m-d H:i");
-define("ITEMSPERPAGE", 25);
-define("ONLINELASTMINUTES", 10);
-define("HEADERERROR", "HTTP/1.1 500 Internal Server Booboo");
-define("UNKNOWN_COUNTRY", "HZ");
-
-//Require
-include 'class/DataBase.php';
-include 'class/AltoRouter.php';
-require 'steamauth/steamauth.php';
-require 'class/SourceQuery/bootstrap.php';
-include 'lang/'.LANGUAGE.'.php';
-include 'model/navigation.php';
-
-$db = new DataBase();
-use xPaw\SourceQuery\SourceQuery;
-$Query = new SourceQuery();
-$Source = SourceQuery::SOURCE;
+include 'includes.php';
 
 //Show errors | disable when live
 if(DEVELOPERMOD == 1) {
@@ -92,7 +72,6 @@ if(isset($CurrentURL))
 else
     $CurrentURL = WEBSITE;
 
-require 'model/global_functions.php';
 require 'partial/header.php';
 
 
