b6568db7d531efbc4dff2c699f6c1fd87cc12c59
Vortéx! <emreires42@gmail.com>
2017-12-11 18:08:25 +0300

    Add Turkish Translation
    Add Turkish Translation.

---
+++ WEB/lang/tr.php
@@ -0,0 +1,154 @@
+<?php
+
+    //LANGUAGE FILE
+    //--TURKISH
+    //Made by Vortéx! (EMRE İREŞ)
+    //Some translations may have errors. It is because of the difference of Turkish-English sentence structure.
+
+    //NAVIGATION
+    define("NAV_HOME", 					"anasayfa");
+    define("NAV_SETTINGS",              "sayfa ayarları");
+    define("NAV_ANNOUNCEMENTS",         "duyurular");
+    define("NAV_COMMUNITYBANS",         "topluluk yasaklamaları");
+    define("NAV_SERVERBANS",            "sunucu yasaklamaları");
+    define("NAV_MUTEGAG",               "sessizleştirme susturma");
+    define("NAV_USERS", 				"kullanıcılar");
+    define("NAV_ADMINS", 				"sunucu yetkilileri");
+    define("NAV_SERVERS", 	    	    "sunucular");
+    define("NAV_CHAT",                  "sohbet");
+    define("NAV_ADD_SERVER", 			"sunucu ekle");
+    define("NAV_ADDPANELADMINS",        "panel yetkilisi ekle");
+    define("NAV_ADDSERVERADMINS",       "sunucu yetkilisi ekle");
+    define("NAV_ADMINONLINETIME",       "yetkili aktiflik süreleri");
+    define("NAV_ADMINCOMMANDS",         "yetkili kayıtları");
+    define("NAV_ADDSADMINGROUPS",       "grup ekle");
+    define("NAV_JUST_ONLINE",           "çevrimiçi");
+    define("NAV_MOSTACTIVE",            "en aktif");
+
+
+    define("WELCOME",                   "hoşgeldin");
+    define("WAS",                       "oldu");
+    define("YOU",                       "sen");
+    define("LOGOUT",                    "çıkış");
+    define("BANS",                      "yasaklamalar"); //multiple
+    define("BAN",                       "yasaklama"); //one
+    define("BANNED",                    "yasaklandı");
+    define("KICK",                      "atılma");
+    define("KICKED",                    "atıldı");
+    define("MUTE",                      "sessizleştirme");
+    define("GAG",                       "susturma");
+    define("SEARCH",                    "ara");
+    define("ADD",                       "ekle");
+    define("ADDED",                     "eklendi");
+    define("ACTIVE",                    "aktif");
+    define("EXPIRED",                   "süresi doldu");
+    define("PLAYER",                    "oyuncu");
+    define("PLAYERS",                   "oyuncular");
+    define("REASON",                    "sebep");
+    define("SERVER",                    "sunucu");
+    define("WEEK",                      "hafta");
+    define("WEEKS",                     "haftalar");
+    define("DAYS",                      "günler");
+    define("DAY",                       "gün");
+    define("HOUR",                      "saat");
+    define("HOURS",                     "saatler");
+    define("MINUTES",                   "dakikalar");
+    define("ADMIN",                     "yetkili");
+    define("LATEST",                    "son");
+    define("LENGTH",                    "süre");
+    define("EDIT",                      "düzenle");
+    define("EDITED",                    "düzenlendi");
+    define("DELETE",                    "sil");
+    define("DELETED",                   "silindi");
+    define("ENDS",                      "bitecek"); // ban "ends"
+    define("AT",                        "de");
+    define("COPY",                      "panoya kopyala");
+    define("ANYSERVER",                 "herhangi bir sunucu");
+    define("PANELADMINS",               "tüm panel yetkilileri");
+    define("NOTFOUND",                  "bulunamadı");
+    define("PANELADMINDELETED",         "panel yetkilisi sildiniz");
+    define("PANELADMINADDED",           "yeni panel yetkilisi eklediniz");
+    define("NOACCESS",                  "bu sayfaya erişiminiz yok");
+    define("RESULTS",                   "sonuçlar");
+    define("TOTAL",                     "toplam");
+    define("SHOWINGRESULTS",            "sonuçlar gösteriliyor");
+    define("NEXT",                      "ileri");
+    define("PREVIUS",                   "geri");
+    define("SELECTACTION",              "eylemi seç");
+    define("REFRESH",                   "yenile");
+    define("ONLINE",                    "aktif");
+    define("ERROR",                     "hata");
+    define("SUCCESS",                   "başarılı");
+    define("SUCCESSFULLY",              "başarıyla");
+    define("WARNING",                   "uyarı");
+    define("NOW",                       "şuan");
+    define("PASTDAY",                   "geçmiş gün");
+    define("THISMONTH",                 "bu ay");
+    define("DATA",                      "veri");
+    define("KILLS",                     "öldürmeler");
+    define("DEATHS",                    "ölümler");
+    define("ACTION",                    "eylem");
+    define("GROUP",                     "grup");
+    define("NAME",                      "isim");
+    define("PASSWORD",                  "şifre");
+    define("IMMUNITY",                  "dokunulmazlık");
+    define("USE1",                      "kullanım");
+    define("TIME",                      "zamanı");
+    define("FLAGS",                     "bayraklar");
+    define("FLAG",                      "bayrak");
+    define("FLAG_CUSTOM",               "Özel Grup");
+    define("RESERVATION",               "rezervasyon");
+    define("GENERIC",                   "genel");
+    define("UNBAN",                     "yasak açma");
+    define("RESTORE",                   "geri yükle");
+    define("RESTORED",                  "geri yüklendi");
+    define("REBAN",                     "tekrar yasakla");
+    define("UNBANNED",                  "yasak kaldırıldı");
+    define("SLAY",                      "katletme");
+    define("CHANGEMAP",                 "harita değiştirme");
+    define("CVAR",                      "cvar komutları");
+    define("CONFIG",                    "yapılandırma");
+    define("CHAT",                      "sohbet");
+    define("VOTE",                      "oylama");
+    define("RCON",                      "rcon komutları");
+    define("CHEATS",                    "cheats komutları");
+    define("ROOT",                      "root");
+    define("ALL",                       "hepsi");
+    define("ANY",                       "hiçbiri");
+    define("LAST",                      "son");
+    define("FIRST",                     "ilk");
+    define("ABOUT",                     "hakkında");
+    define("CONNECTIONS",               "bağlantılar");
+    define("DELETECONFIRM",             "silmek istediğinize emin misiniz?");
+    define("DELETECONFIRMTXT",          "eğer bu grubu silerseniz, bu gruptaki tüm yetkililer de silinecek!");
+    define("CANCEL",                    "iptal");
+    define("CONFIRM",                   "onayla");
+    define("NOWEDITING",                "şuan düzenliyorsunuz");
+    define("PLEASEENTER",               "lütfen girin");
+    define("ONLYLETTERS",               "sadece harfler");
+    define("MUSTBENUMBER",              "sayı olmalı");
+    define("NAMEEXISTS",                "böyle bir isim zaten var! lütfen başka bir tane kullanın!");
+    define("LOGS",                      "kayıtlar");
+    define("COMMAND",                   "komut");
+    define("PERMANENET",                "kalıcı");
+    define("BEFORE",                    "önce");
+    define("MUTEGAG",                   "mutegag");
+    define("SILENCE",                   "silence");
+    define("THEREWASERROR",             "bir hata oluştu");
+    define("PLAYERNOTFOUND",            "Oyuncu bulunamadı, lütfen steamID64, steamID32 veya steam topluluk profil adresini kullanınız");
+    define("ENTERPLAYER",               "Lütfen oyuncunun alanını giriniz");
+    define("TYPE",                      "tür");
+    define("MORE",                      "daha");
+    define("OPTIONS",                   "ayarlar");
+    define("SELECT",                    "seç");
+    define("UNKNOWN",                   "bilinmeyen");
+    define("MESSAGE",                   "mesaj");
+    define("SEND",                      "gönder");
+    define("TO",                        "'ye");
+    define("TIMELEFT",                  "kalan zaman");
+    define("SCORE",                     "skor");
+
+    //Errors
+    define("SERVER_ERROR",              "JSON verileri sunucudan çözülemedi");
+
+?>
