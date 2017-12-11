235d32b433c0af19c203d077ae10938b0b36e2b2
Hexah <hexer504@gmail.com>
2017-12-11 17:02:02 +0100

    Added italian translation.

---
+++ WEB/lang/it.php
@@ -0,0 +1,154 @@
+<?php
+
+    //LANGUAGE FILE
+    //--ENGLISH
+    //Made by Hexah(Papero)
+
+
+    //NAVIGATION
+    define("NAV_HOME", 					"home");
+    define("NAV_SETTINGS",              "impostazioni pagina");
+    define("NAV_ANNOUNCEMENTS",         "annunci");
+    define("NAV_COMMUNITYBANS",         "bans della community");
+    define("NAV_SERVERBANS",            "bans del server");
+    define("NAV_MUTEGAG",               "mute gag");
+    define("NAV_USERS", 				"utenti");
+    define("NAV_ADMINS", 				"admin del server");
+    define("NAV_SERVERS", 	    	    "servers");
+    define("NAV_CHAT",                  "chat");
+    define("NAV_ADD_SERVER", 			"aggiungi server");
+    define("NAV_ADDPANELADMINS",        "aggiungi admin pannello");
+    define("NAV_ADDSERVERADMINS",       "aggiungi admin server");
+    define("NAV_ADMINONLINETIME",       "tempo online admin");
+    define("NAV_ADMINCOMMANDS",         "log admin");
+    define("NAV_ADDSADMINGROUPS",       "aggiungi gruppi admin");
+    define("NAV_JUST_ONLINE",           "solo online");
+    define("NAV_MOSTACTIVE",            "più attivi");
+
+
+    define("WELCOME",                   "benvenuto");
+    define("WAS",                       "eri");
+    define("YOU",                       "tu");
+    define("LOGOUT",                    "logout");
+    define("BANS",                      "bans"); //multiple
+    define("BAN",                       "ban"); //one
+    define("BANNED",                    "bannato");
+    define("KICK",                      "kick");
+    define("KICKED",                    "kicked");
+    define("MUTE",                      "mute");
+    define("GAG",                       "gag");
+    define("SEARCH",                    "cerca");
+    define("ADD",                       "aggiungi");
+    define("ADDED",                     "aggoimto");
+    define("ACTIVE",                    "attivo");
+    define("EXPIRED",                   "finito");
+    define("PLAYER",                    "giocatore");
+    define("PLAYERS",                   "giocatori");
+    define("REASON",                    "motivo");
+    define("SERVER",                    "server");
+    define("WEEK",                      "settimana");
+    define("WEEKS",                     "settimane");
+    define("DAYS",                      "giorni");
+    define("DAY",                       "giorno");
+    define("HOUR",                      "ora");
+    define("HOURS",                     "ore");
+    define("MINUTES",                   "minuti");
+    define("ADMIN",                     "admin");
+    define("LATEST",                    "ultimo");
+    define("LENGTH",                    "lunghezza");
+    define("EDIT",                      "modifica");
+    define("EDITED",                    "modificato");
+    define("DELETE",                    "elimina");
+    define("DELETED",                   "eliminatp");
+    define("ENDS",                      "finito"); // ban "ends"
+    define("AT",                        "in");
+    define("COPY",                      "copia negli appunti");
+    define("ANYSERVER",                 "qualsiasi server");
+    define("PANELADMINS",               "qualsiasi admin de pannello");
+    define("NOTFOUND",                  "non trova");
+    define("PANELADMINDELETED",         "hai eliminato un admin del pannello");
+    define("PANELADMINADDED",           "hai aggiunto un admin del pannello");
+    define("NOACCESS",                  "non hai accesso a questa pagina");
+    define("RESULTS",                   "risultati");
+    define("TOTAL",                     "totale");
+    define("SHOWINGRESULTS",            "mostrando i risultati di");
+    define("NEXT",                      "successivo");
+    define("PREVIUS",                   "precedente");
+    define("SELECTACTION",              "seleziona azione");
+    define("REFRESH",                   "aggiorna");
+    define("ONLINE",                    "online");
+    define("ERROR",                     "errore");
+    define("SUCCESS",                   "sucesso");
+    define("SUCCESSFULLY",              "con successo");
+    define("WARNING",                   "attenzione");
+    define("NOW",                       "ora");
+    define("PASTDAY",                   "giorni precendenti");
+    define("THISMONTH",                 "questo pese");
+    define("DATA",                      "data");
+    define("KILLS",                     "uccisioni");
+    define("DEATHS",                    "morti");
+    define("ACTION",                    "azione");
+    define("GROUP",                     "gruppo");
+    define("NAME",                      "nome");
+    define("PASSWORD",                  "password");
+    define("IMMUNITY",                  "immunità");
+    define("USE1",                      "usa");
+    define("TIME",                      "tempo");
+    define("FLAGS",                     "flags");
+    define("FLAG",                      "flag");
+    define("FLAG_CUSTOM",               "Gruppo personalizzato");
+    define("RESERVATION",               "reservation");
+    define("GENERIC",                   "generic");
+    define("UNBAN",                     "unban");
+    define("RESTORE",                   "ripristina");
+    define("RESTORED",                  "ripristinato");
+    define("REBAN",                     "reban");
+    define("UNBANNED",                  "unbanned");
+    define("SLAY",                      "slay");
+    define("CHANGEMAP",                 "changemap");
+    define("CVAR",                      "cvar");
+    define("CONFIG",                    "config");
+    define("CHAT",                      "chat");
+    define("VOTE",                      "vote");
+    define("RCON",                      "rcon");
+    define("CHEATS",                    "cheats");
+    define("ROOT",                      "root");
+    define("ALL",                       "tutte");
+    define("ANY",                       "nessuna");
+    define("LAST",                      "ultima");
+    define("FIRST",                     "prima");
+    define("ABOUT",                     "about");
+    define("CONNECTIONS",               "connesioni");
+    define("DELETECONFIRM",             "sei sicuro di voler eliminare?");
+    define("DELETECONFIRMTXT",          "se elmini questo gruppo, tutti gli admin che si trovano dentro il gruppo verrano elminati!");
+    define("CANCEL",                    "cancella");
+    define("CONFIRM",                   "conferma");
+    define("NOWEDITING",                "ora stai motificando");
+    define("PLEASEENTER",               "per favore insersci");
+    define("ONLYLETTERS",               "solo lettere");
+    define("MUSTBENUMBER",              "deve essere un numbero");
+    define("NAMEEXISTS",                "questo nome esiste già! Per favore usane un altro!");
+    define("LOGS",                      "logs");
+    define("COMMAND",                   "comandi");
+    define("PERMANENET",                "permanente");
+    define("BEFORE",                    "prima");
+    define("MUTEGAG",                   "mutegag");
+    define("SILENCE",                   "silence");
+    define("THEREWASERROR",             "c'è stato un error");
+    define("PLAYERNOTFOUND",            "Player non trovato, usare steamID64, steamID32 o steamcommunity profile URL!");
+    define("ENTERPLAYER",               "Per favore completa il compo player");
+    define("TYPE",                      "tipo");
+    define("MORE",                      "più");
+    define("OPTIONS",                   "impostazioni");
+    define("SELECT",                    "seleziona");
+    define("UNKNOWN",                   "sconosciuto");
+    define("MESSAGE",                   "messaggio");
+    define("SEND",                      "invia");
+    define("TO",                        "a");
+    define("TIMELEFT",                  "tempo rimasto");
+    define("SCORE",                     "score");
+
+    //Errors
+    define("SERVER_ERROR",              "impossibile decifrare JSON data dal server!");
+
+?>
